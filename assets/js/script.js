665227c7bda4eca94a4786bffd24b6cf7ca85feb
Derrick Hepworth <derrickhepworth@gmail.com>
2020-08-25 11:32:18 -0600

    psuedocode and initial vars

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,15 @@
-alert("Javascript file linked")
+//Environment Variables
+var pageContentel = document.querySelector("#page-content");
+
+//Functions
+
+//Start Button
+ var startButtonHandler = function () {};
+ // start timer
+ // load html questions
+    // multiple choice answer form
+ // load timer visual
+
+
+//Event Listeners
+pageContentel.addEventListener("click", startButtonHandler);
