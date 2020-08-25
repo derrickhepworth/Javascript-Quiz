6bbf9469664e8b8ee73e30e41765bca580ac65ce
Derrick Hepworth <derrickhepworth@gmail.com>
2020-08-25 14:05:37 -0600

    working timer display

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,12 @@
 //Environment Variables
+var headerContentEl = document.querySelector("#header");
 var pageContentEl = document.querySelector("#page-content");
 var startQuizEl = document.querySelector("#start-quiz");
 var startQuizDivEl = document.querySelector("#start-info");
 var userScore = 0;
 var questionId = 0;
 var questionText = "This is just the test text for the question YO.";
+var second = 60;
 
 //Functions
 
@@ -17,30 +19,51 @@ var loadQuestion = function () {
 };
 
 var createQuestionEl = function () {
-    console.log ("creatQuestionEl function called");
-var questionEl = document.createElement("div");
-questionEl.className = 'question-box';
-questionEl.setAttribute("data-question-id", questionId);
-questionEl.innerHTML = "<h3 class='question'>" + questionText + "</h3>";
-pageContentEl.appendChild(questionEl);
-console.log("questionId", questionId);
 
-var answerEl = document.createElement("form");
-answerEl.className = 'question-box';
-answerEl.innerHTML = "<input type='radio' id='' class='' value='A' ><label for='A'>A-just getting it working.</label><br><input type='radio' id='' class='' value='B' ><label for='B'>B-Doing what I do.</label><br><input type='radio' id='' class='' value='C' ><label for='C'>C-It's test fodder.</label><br><input type='radio' id='' class='' value='D' ><label for='D'>D-WubbaLubbaDubDub.</label><br>"
-questionEl.appendChild(answerEl);
+    //Question div with text    
+    var questionEl = document.createElement("div");
+    questionEl.className = 'question-box';
+    questionEl.setAttribute("data-question-id", questionId);
+    questionEl.innerHTML = "<h3 class='question'>" + questionText + "</h3>";
+    pageContentEl.appendChild(questionEl);
+    console.log("questionId", questionId);
+
+    //answer div with radio options within Question Div
+    var answerEl = document.createElement("form");
+    answerEl.className = 'question-box';
+    answerEl.innerHTML = "<input type='radio' id='' class='' value='A' ><label for='A'>A-just getting it working.</label><br><input type='radio' id='' class='' value='B' ><label for='B'>B-Doing what I do.</label><br><input type='radio' id='' class='' value='C' ><label for='C'>C-It's test fodder.</label><br><input type='radio' id='' class='' value='D' ><label for='D'>D-WubbaLubbaDubDub.</label><br>"
+    questionEl.appendChild(answerEl);
+
+};
+
+var createTimerEl = function () {
+    var timerEl = document.createElement("h1");
+    timerEl.id = "timer-clock";
+    // timerEl.innerText = ":" + secondDec;
+    headerContentEl.appendChild(timerEl);
+
+    var oneSecond = function () {
+        secondDec = second--;
+        console.log(secondDec);
+        timerEl.innerText= secondDec ;
+    };
+    
+    setInterval(oneSecond, 1000);
 };
 
 //Start Button
 var startButtonHandler = function () {
-    console.log("click event");
     console.log("timer started");
+    createTimerEl();
 
     // Timer
     var gameTimer = function () {
-        alert("Game Over. Your score is " + userScore + '. ');
+        // alert("Game Over. Your score is " + userScore + '. ');
+        console.log("timer ended");
     };
+
     setTimeout(gameTimer, 60000);
+
 
     // load html questions
 
